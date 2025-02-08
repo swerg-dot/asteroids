ba5c1e92c406829d1625a89d9807ed03ec7aad8e
MatthewGarci2 <0831mmg@gmail.com>
2025-02-08 16:08:42 -0500

    B: Starting asteroids!

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants.py import *
 
 def main():
 	print('Starting asteroids!')
-
+	print(f'Screen width: {SCREEN_WIDTH} \n Screen height: {SCREEN_HEIGHT}')
 if __name__ == "__main__":
     main()
